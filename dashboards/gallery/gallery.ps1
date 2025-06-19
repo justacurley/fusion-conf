5db455384e124d7663775d659a0b53dc3b71eced
curleylax <curleylax>
2025-06-19 23:59:54 +0000

    Working gallery! layout is shit but we can work on that

--- dashboards/gallery/gallery.ps1
+++ dashboards/gallery/gallery.ps1
@@ -2,14 +2,16 @@
     New-UDLayout -Columns 2 -content {
         $imagePath = "/home/data/Repository/fusion-data/img"
         $imageFiles = Get-ChildItem -Path $imagePath -File
-    }
-    foreach ($img in $imageFiles) {
-        $relativePath = $img.FullName.Replace($imagePath,"").TrimStart("/")
-        $imageUrl = "/images/$relativePath"
+        
+        foreach ($img in $imageFiles) {
+            $relativePath = $img.FullName.Replace($imagePath,"").TrimStart("/")
+            $imageUrl = "/img/$relativePath"
 
-        New-UDCard -Content {
-            New-UDImage -url $imageUrl -Width 200 -Height 200
-            New-UDTypography -Text $img.Name -Variant subtitle1
-        } -Elevation 2
+            New-UDCard -Content {
+                New-UDImage -url $imageUrl -Width 200 -Height 200
+                New-UDTypography -Text $img.Name -Variant subtitle1
+            } -Elevation 2
+        }
     }
 }
+
