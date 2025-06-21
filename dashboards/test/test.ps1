951de81de69d53f68bb024d5b616b7ea1df311f7
curleylax <curleylax>
2025-06-21 03:55:55 +0000

    working on creating list from json file

--- dashboards/test/test.ps1
+++ dashboards/test/test.ps1
@@ -1,20 +1,8 @@
 ﻿New-UDApp -Content {
     New-UDContainer -Content {
         New-UDTypography -Text "Health Recovery Entry Form" -Variant h4 -Style @{marginBottom = "20px" }
-        New-UDTypography -Text "Entry Date and Time" -Variant h6 -Style  @{marginTop = "20px"; marginBottom="10px"}
+        New-UDTypography -Text "Entry Date and Time" -Variant h6 -Style  @{marginTop = "20px"; marginBottom = "10px" }
         New-UDForm -Content {
-            # Get Medication Data
-            try {
-                $MedData = Get-Content -Path "/home/data/Repository/fusion-data/entries/schema.json" | ConvertFrom-Json
-                $Dosages = $MedData.Medications
-                Show-UDModal -Content {
-                    New-UDTypography -Text "Json data loaded"
-                    New-UDJson -json ($Dosages | ConvertTo-Json)
-                }
-
-            } catch {
-                Write-Error "Failed to get or parse data $_"
-            }
             # Date and Time fields
             New-UDGrid -Container -Content {
                 $MSTDate = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date), 'Mountain Standard Time') | ForEach-Object { $_.ToString("MMdd HHmm") }
@@ -28,15 +16,27 @@
                 }
             }
             # Medicatins Sectin
-            New-UDTypography -Text "Medications" -Variant h6 -Style @{marginTop = "20px"}
+            New-UDTypography -Text "Medications" -Variant h6 -Style @{marginTop = "20px" }
+            
             New-UDSelect -Id "meds" -Option {
-                New-UDSelectOption -Name "opt1" -Value "5mg"
-                New-UDSelectOption -Name "opt2" -Value "10mg"
-                New-UDSelectOption -Name "opt3" -Value "20mg"
-            } -Multiple -PlaceHolder "Select Options" -OnChange {
-                $EventData = $body | ConvertFrom-Json 
-                Show-UDToast -Message "selected $($EventData -join ","")"
-            }
+                # Get Medication Data
+                try {
+                    $MedData = Get-Content -Path "/home/data/Repository/fusion-data/entries/schema.json" | ConvertFrom-Json -AsHashtable
+                    $Dosages = $MedData['Medications']
+                    ##$EmptyEntry = $MedData['EmptyEntry']
+                }
+                catch {
+                    Write-Error "Failed to get or parse data $_"
+                }
+                for ($key -in $Dosages.keys) {
+                    Show-UDToast -Message $key -Duration 5
+                    New-UDSelectOption -Name $key -Value "poop"
+                }               
+            } -Multiple
+            # -Multiple -PlaceHolder "Select Options" -OnChange {
+            #     $EventData = $body | ConvertFrom-Json 
+            #     Show-UDToast -Message "selected $($EventData -join ","")"
+            # }
         } -OnSubmit {
             param($Data)
             Write-Output $Data
