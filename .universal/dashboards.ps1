67dd6580e4b0732128a7c2989a7ee119f3ef385d
curleylax <curleylax>
2025-06-23 01:06:15 +0000

    adding testb for smaller test jobs. still can't figure out dynamically creating elements

--- .universal/dashboards.ps1
+++ .universal/dashboards.ps1
@@ -1,3 +1,4 @@
 ﻿New-PSUApp -Name "entries" -FilePath "dashboards/Entries/Entries.ps1" -BaseUrl "/entries" -Environment "PowerShell 7" -AutoDeploy -Description "Add or update entries" 
 New-PSUApp -Name "gallery" -FilePath "dashboards/gallery/gallery.ps1" -BaseUrl "/gallery" -AutoDeploy -Description "Gallery of incision healing" 
-New-PSUApp -Name "testapp" -FilePath "dashboards/test/test.ps1" -BaseUrl "/testapp" -Environment "PowerShell 7" -AutoDeploy -Description "test stuff"
+New-PSUApp -Name "testapp" -FilePath "dashboards/test/test.ps1" -BaseUrl "/testapp" -Environment "PowerShell 7" -AutoDeploy -Description "test stuff" 
+New-PSUApp -Name "testb" -FilePath "dashboards/testb/testb.ps1" -BaseUrl "/testb" -Environment "PowerShell 7" -AutoDeploy
---
+++ dashboards/testb/testb.ps1
@@ -0,0 +1,39 @@
+﻿New-UDApp -Content { 
+    
+    New-UDButton -Text "Add Controls" -OnClick {
+
+        New-udform -Id 'dynamic-form' -Content {
+            # Initial controls
+            New-UDRow -Id "ControlContainer" -Content {
+                New-UDColumn -Content {
+                    New-UDSelect -Id "Dropdown1" -Option { 
+                        @(
+                            New-UDSelectOption -Name "Option 1" -Value "Option1", 
+                            New-UDSelectOption -Name "Option 2" -Value "Option2", 
+                            New-UDSelectOption -Name "Option 3" -Value "Option3" 
+                        )
+                    }
+                    New-UDTextbox -Id "Textbox1"
+                }
+            }
+        } -Endpoint {
+            $count = (Get-UDElement -Id "ControlContainer").Content.Count
+
+            # Adding new controls
+            New-UDRow -Content {
+                New-UDColumn -Content {
+                    New-UDSelect -Id "Dropdown$count" -Option { 
+                        @( 
+                            New-UDSelectOption -Name "Option 1" -Value "Option1", 
+                            New-UDSelectOption -Name "Option 2" -Value "Option2", 
+                            New-UDSelectOption -Name "Option 3" -Value "Option3" 
+                        )
+                    }
+                    New-UDTextbox -Id "Textbox$count"
+                }
+            } | Add-UDElement -Id "ControlContainer"
+        }
+    } 
+}
+
+
