90d001f95e5506a7dfc0660b09d4ffb2d837cc5c
Alex Curley <curleylax@gmail.com>
2025-06-24 23:04:52 -0600

    add function to manually add entries to json for backlog purposes

--- Modules/fusion/fusion.psm1
+++ Modules/fusion/fusion.psm1
@@ -1,100 +1,6 @@
 $global:EntriesPath =  "/home/data/fusion-data/entries/entries.json"
 
-function Add-Entry {
-    [CmdletBinding()]
-    param (
-        [string]$Date = (Get-Date -f "MMdd"),
-        [string]$Time = (Get-Date -f "HHmm"),
-        [parameter()]
-        [ValidateSet("tylenol1", "dilaudid4", "valium5", "vitaminD", "lexapro2", "lexapro1", "journavx", "oxycodone")]
-        [string[]]$Medications,
-        [string]$ScarImage,
-        [parameter(Mandatory = $false)]
-        [string[]]$Activities,
-        [parameter()]
-        # [ValidateScript({ $_.Count -eq $Activities.Count })]
-        [int[]]$ActivitiesDuration,
-        [string[]]$PainLocation,
-        [string[]]$PainLevel,
-        [string]$o2,
-        [string]$bpr,
-        [string]$Note,
-        [string]$Sleep
-    )    
-    begin {
-        # Save and set InformationAction
-        $CurrentInformationAction = $InformationPreference
-        $InformationPreference = 'Continue'
-        # Do some param validation we cant do in ValidateScript
-        if (($Activities -and $ActivitiesDuration) -and ($Activities.Count -ne $ActivitiesDuration.Count)) {
-            throw "Activities Count ne ActivitiesDuration $Activities : $($ActivitiesDuration -join ",")"
-        }
-        if (($PainLocation -and $PainLevel) -and ($PainLocation.Count -ne $PainLevel.Count)) {
-            write-host $PainLevel
-            throw "PainLocation Count ne PainLevel $PainLocation : $($PainLevel -join ",")"
-        }
-    }
-    end {
-        $Schema = Get-Content -Path $SchemaPath | ConvertFrom-Json -AsHashtable
-        $Entries = Get-Content -Path $Entriespath | ConvertFrom-Json -AsHashtable
-        # Add Date and Time keys
-        if ($Date -notin $Entries.keys) {
-            $Entries.add($Date,@{})
-        }
-        if ($Time -notin $Entries[$Date].keys) {
-            $Entries[$Date].add($Time,@{})           
-        }
-        $Entries[$Date][$Time]=$Schema["EmptyEntry"]
-        $CurrentEntry = $Entries[$Date][$Time]
-        # Add medications
-        $Medications.ForEach({
-                $MedName = ($_ -replace '\d', '')
-                $CurrentEntry["Medications"][$MedName] = $schema["Medications"][$_]
-            })
-        # Add activities
-        $AllActivities = @{}
-        for ($i = 0; $i -lt $Activities.Count; $i++) {
-            $AllActivities.Add($Activities[$i], $ActivitiesDuration[$i])
-        }
-        $CurrentEntry["Activities"] = $AllActivities
-        # Add pain
-        $AllPain = @{}
-        for ($i = 0; $i -lt $PainLocation.Count; $i++) {
-            $AllPain.Add($PainLocation[$i], $PainLevel[$i])
-        }
-        $CurrentEntry["Pain"] = $AllPain
-        # Add o2, bpr, notes
-        $CurrentEntry["o2"] = $o2
-        $CurrentEntry["bpr"] = $bpr
-        $CurrentEntry["note"] = $Note
-        if ($Sleep -and ('Sleep' -notin $Entries[$Date].keys)) {
-            $Entries[$Date]['Sleep'] = $Sleep
-        }
-        if ($ScarImage -and ('ScarImage' -notin $Entries[$Date].keys)) {
-            $Entries[$Date]['ScarImage'] = $ScarImage
-        }
-        
-        # Create the output structure compatible with Save-ConvertedEntry
-        $Result = @{
-            FullEntry = @{
-                $Date = @{
-                    $Time = $CurrentEntry
-                }
-            }
-            EntryStructure = $CurrentEntry
-            Date = $Date
-            Timestamp = $Time
-        }
-        
-        # Restore InformationAction
-        $InformationPreference = $CurrentInformationAction
-        
-        return $Result
-    }
-}
-Set-Alias -Name ae -Value Add-Entry
-Export-ModuleMember -Function Add-Entry -Alias ae
-# Add-Entry -Time 1300 -Medications dilaudid4 -PainLocation back -PainLevel 5-6 -o2 90 -bpr 120/80 -Note "short walk this morning, didn't increase pain"
+
 
 function ConvertTo-EntriesFormat {
     param(
@@ -513,3 +419,113 @@ function Sync-EntriesToDynamoDB {
     }
 }
 Export-ModuleMember -Function Sync-EntriesToDynamoDB
+
+function New-EntryObject {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Date,
+        
+        [Parameter(Mandatory = $true)]
+        [string]$Timestamp,
+        
+        [Parameter(Mandatory = $false)]
+        [hashtable]$Medications = @{},
+        
+        [Parameter(Mandatory = $false)]
+        [hashtable]$Pain = @{},
+        
+        [Parameter(Mandatory = $false)]
+        [hashtable]$Activities = @{},
+        
+        [Parameter(Mandatory = $false)]
+        [string]$O2 = "",
+        
+        [Parameter(Mandatory = $false)]
+        [string]$BPR = "",
+        
+        [Parameter(Mandatory = $false)]
+        [string]$Note = ""
+    )
+    
+    begin {
+        # Save and set InformationAction
+        $CurrentInformationAction = $InformationPreference
+        $InformationPreference = 'Continue'
+        
+        Write-Information "Creating new entry object for $Date at $Timestamp"
+        
+        # Validate date format (MMDD)
+        if ($Date -notmatch '^\d{4}$') {
+            throw "Date must be in MMDD format (e.g., '0624')"
+        }
+        
+        # Validate timestamp format (HHMM)
+        if ($Timestamp -notmatch '^\d{4}$') {
+            throw "Timestamp must be in HHMM format (e.g., '1430')"
+        }
+    }
+    
+    process {
+        try {
+            # Calculate GSI fields
+            $medicationTaken = if ($Medications.Keys.Count -gt 0) { 
+                ($Medications.Keys -join ",") 
+            } else { "" }
+            
+            $maxPainLevel = 0
+            if ($Pain.Values.Count -gt 0) {
+                foreach ($level in $Pain.Values) {
+                    $numbers = $level -split '[-,\s]' | Where-Object { $_ -match '^\d+$' } | ForEach-Object { [int]$_ }
+                    if ($numbers) {
+                        $levelMax = ($numbers | Measure-Object -Maximum).Maximum
+                        if ($levelMax -gt $maxPainLevel) { $maxPainLevel = $levelMax }
+                    }
+                }
+            }
+            
+            # Create the entry structure with DynamoDB GSI fields
+            $EntryStructure = @{
+                "Medications" = $Medications
+                "Pain" = $Pain
+                "Activities" = $Activities
+                "o2" = $O2
+                "bpr" = $BPR
+                "note" = $Note
+                "medication_taken" = $medicationTaken
+                "max_pain_level" = $maxPainLevel
+            }
+            
+            # Create the full structure for entries.json
+            $FullEntry = @{
+                $Date = @{
+                    $Timestamp = $EntryStructure
+                }
+            }
+            
+            # Create the result object compatible with Save-ConvertedEntry
+            $Result = @{
+                FullEntry = $FullEntry
+                EntryStructure = $EntryStructure
+                Date = $Date
+                Timestamp = $Timestamp
+            }
+            
+            Write-Information "Created entry object - Meds: '$medicationTaken', Max Pain: $maxPainLevel"
+            
+            # Restore InformationAction
+            $InformationPreference = $CurrentInformationAction
+            
+            return $Result
+            
+        } catch {
+            Write-Error "Error creating entry object: $($_.Exception.Message)"
+            
+            # Restore InformationAction even on error
+            $InformationPreference = $CurrentInformationAction
+            
+            throw
+        }
+    }
+}
+Export-ModuleMember -Function New-EntryObject
