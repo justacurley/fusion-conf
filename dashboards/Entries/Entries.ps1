3d8211069d4a9909c55e414f91bf888d13f5fff0
Alex Curley <curleylax@gmail.com>
2025-06-25 18:20:47 -0600

    fix entries img upload code

--- dashboards/Entries/Entries.ps1
+++ dashboards/Entries/Entries.ps1
@@ -171,7 +171,7 @@
             # Upload an image 
             New-UDGrid -Container -Content {
                 New-UDGrid -Item -ExtraSmallSize 12 -Content {
-                    New-UDFileUpload -Id "ImageFile" -Label "Upload Image (optional)" -Accept "image/*" -FullWidth
+                    New-UDUpload -Id "ImageFile" -Label "Upload Image (optional)" -Accept "image/*" -FullWidth
                 }
             }
         } -OnSubmit {
