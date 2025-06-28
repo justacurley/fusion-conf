7a93e7576f46acc10c2b562804dd61f6fcba4f54
Alex Curley <curleylax@gmail.com>
2025-06-28 15:15:29 -0600

    add debugger

--- dashboards/Entries/Entries.ps1
+++ dashboards/Entries/Entries.ps1
@@ -1,4 +1,5 @@
 ﻿New-UDApp -Content {
+    Wait-Debugger
     New-UDContainer -Content {
         New-UDPaper -Children {
             New-UDGrid -Container -Content {
@@ -151,7 +152,7 @@
                                 
                                 # Add More Button - Styled
                                 New-UDGrid -Item -ExtraSmallSize 12 -Content {
-                                    Wait-Debugger
+                                    
                                     New-UDButton -Text "➕ Add Another Activity" -Color primary -Variant outlined -OnClick {
                                         # Add another activities entry row
                                         $entryCount = (Get-Random -Minimum 100 -Maximum 999)
