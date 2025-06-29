134f154431555cd38eb6171f9e4f9826f2a3613f
curleylax <curleylax>
2025-06-29 02:08:29 +0000

    Added script that will copy live "database" to repo

---
+++ fusion-data/scripts/copy_entries.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop"
+$timestamp = Get-Date -f yyyyMMdd-HHmm
+try {
+    Get-Item /home/data/fusion-data/entries/entries.json -OutVariable entries
+    Copy-Item $entries "/home/data/Repository/fusion-data/entries_$timestamp.json" -Force
+} catch {
+    Write-Information "Failed to get or copy /home/data/fusion-data/entries/entries.json"
+    throw $_
+}
