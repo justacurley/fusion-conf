823f09b45cd556f46212b010beb4677c61bf8a2a
curleylax <curleylax>
2025-06-29 20:59:54 +0000

    add a script to test cache

---
+++ fusion-data/scripts/testcache.ps1
@@ -0,0 +1,9 @@
+﻿# Script contents
+param($key)
+$DNE = Get-PSUCache -Key $Key
+if ( -not $DNE) {
+    Write-Information "no $Key cache"
+} else {
+    Write-Information "Found $Key cache"
+    $DNE | Get-Member
+ }
