fcf3b23adcf3d17dafaab581f35962f20e8c3bfe
curleylax <curleylax>
2025-06-30 01:33:13 +0000

    hold

--- dashboards/test_calendars/test_calendars.ps1
+++ dashboards/test_calendars/test_calendars.ps1
@@ -2,15 +2,13 @@
     New-UDContainer -Content {
         New-UDTypography -Text "Test Calendar Charts" -Variant h4 -Align center
         Import-Module -Name GetFusion -Force
-        Clear-CacheData
-        $EntriesPath = "/home/data/fusion-data/entries/entries.json"
-        try {
-            $Entries = Get-EntriesData -Path $EntriesPath
-            Set-PSUCache -Key "Entries" -Value $Entries -Expiration (New-TimeSpan -Days 1)
-        }
-        catch {
-            <#Do this if a terminating exception happens#>
-        }
+        Clear-CachedData
+        $Entries = (Get-PSUCacheData -Key 'entriesData' -OutVariable TempEntry) ? $TempEntry : (& {
+                $EntriesPath = "/home/data/fusion-data/entries/entries.json"
+                Get-EntriesData -Path $EntriesPath
+                Set-PSUCache -Key "Entries" -Value $Entries -Expiration (New-TimeSpan -Days 1) | Out-Null
+            })
+
         $Data = @()
         for ($i = 365; $i -gt 0; $i--) {
             $Data += @{
