109ac0ac6f60776016332160285e431beeabcde6
curleylax <curleylax>
2025-06-30 01:40:23 +0000

    ternary cache

--- dashboards/test_calendars/test_calendars.ps1
+++ dashboards/test_calendars/test_calendars.ps1
@@ -3,11 +3,14 @@
         New-UDTypography -Text "Test Calendar Charts" -Variant h4 -Align center
         Import-Module -Name GetFusion -Force
         Clear-CachedData
-        $Entries = (Get-PSUCacheData -Key 'entriesData' -OutVariable TempEntry) ? $TempEntry : (& {
+        $Entries = (Get-PSUCache -Key 'entriesData' -OutVariable TempEntry) ? $TempEntry : (& {
+                Write-Information "Could not find entriesData cache"
                 $EntriesPath = "/home/data/fusion-data/entries/entries.json"
                 Get-EntriesData -Path $EntriesPath
                 Set-PSUCache -Key "Entries" -Value $Entries -Expiration (New-TimeSpan -Days 1) | Out-Null
             })
+        $TempEntry ? (Remove-Variable TempEntry) : (Out-Null)
+
 
         $Data = @()
         for ($i = 365; $i -gt 0; $i--) {
