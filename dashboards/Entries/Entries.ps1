06499d42a826b617d53b4030e2e47aabe3443054
Alex Curley <curleylax@gmail.com>
2025-07-09 22:13:56 -0600

    check out env vars

--- dashboards/Entries/Entries.ps1
+++ dashboards/Entries/Entries.ps1
@@ -130,7 +130,7 @@ function New-ActivityEntryElement {
 }
 New-UDApp -Content {
     
-    Write-Information "Testing Session Variable UserRaw: $(Get-Variable | Convertto-json -depth 2)"
+    Write-Information "Testing Session Variable UserRaw: $(Get-Variable | Convertto-json -depth 99)"
     New-UDContainer -Children {
         New-UDPaper -Children {
             New-UDGrid -Container -Children {
