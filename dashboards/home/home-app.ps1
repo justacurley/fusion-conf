05650801223f11639336a6de8f7e92916bb666b4
Alex Curley <curleylax@gmail.com>
2025-07-09 21:11:27 -0600

    capture output of get-currentuser

--- dashboards/home/home-app.ps1
+++ dashboards/home/home-app.ps1
@@ -1,6 +1,13 @@
 ﻿$HomePage = New-UDApp -Content {
     Import-Module UserManagement -Force
-    Get-CurrentUser
+    $CurrentUser = Get-CurrentUser
+    Write-Information "Get-CurrentUser Output: $($CurrentUser | Convertto-Json -Depth 3)"
+    if (-not $CurrentUser.Success) {
+        Show-UDToast -Message "Authentication issue, redirecting to login page" -MessageColor Red -Duration 2000
+        Invoke-UDRedirect -Url /login -Native
+    } else {
+        $UserData = $CurrentUser.Data
+    }
     Write-Information "User authenticated: $($User.Identity.IsAuthenticated)"
     Write-Information "User name: $($User.Identity.Name)"
     Write-Information "Session UserEmail: $($Session:UserEmail)"
