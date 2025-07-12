be327e0cd29b9ee3439d359eefae30b4ce29269d
justacurley <alexwmcurley@gmail.com>
2025-07-12 00:00:58 -0600

    update settings to use new-udform

--- dashboards/Settings/Settings.ps1
+++ dashboards/Settings/Settings.ps1
@@ -295,8 +295,49 @@ $SettingsPage = New-UDApp -Content {
                 }
 
                 New-UDButton -Text '+ Add Another Medication' -Color success -OnClick {
-                    # TODO: Add logic to dynamically add medication entries
-                    Show-UDToast -Message 'Medication entry added!' -Duration 2000
+                    # Use session variable to track medication count
+                    if (-not $Session:MedicationCounter) { $Session:MedicationCounter = 2 }
+                    $medCount = $Session:MedicationCounter
+                    $Session:MedicationCounter++
+                    
+                    Show-UDToast -Message "Adding Medication #$medCount" -Duration 2000
+                    
+                    # Add new medication entry to the container
+                    Add-UDElement -ParentId 'medications-container' -Content {
+                        $currentMedCount = $medCount  # Capture in local scope
+                        New-UDElement -Id "medication-item-$currentMedCount" -Tag 'div' -Attributes @{ class = 'medication-item' } -Content {
+                            New-UDGrid -Container -Children {
+                                New-UDGrid -Item -ExtraSmallSize 10 -SmallSize 3 -Children {
+                                    New-UDTextbox -Id "med_name_$currentMedCount" -Label 'Medication Name' -FullWidth
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 12 -SmallSize 3 -Children {
+                                    New-UDTextbox -Id "med_dosage_$currentMedCount" -Label 'Dosage (e.g., 10mg)' -FullWidth
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 12 -SmallSize 3 -Children {
+                                    New-UDSelect -Id "med_frequency_$currentMedCount" -Label 'Frequency' -FullWidth -Option {
+                                        New-UDSelectOption -Name 'Once Daily' -Value 'daily'
+                                        New-UDSelectOption -Name 'Twice Daily' -Value 'twice_daily'
+                                        New-UDSelectOption -Name 'Three Times Daily' -Value 'three_times_daily'
+                                        New-UDSelectOption -Name 'As Needed' -Value 'as_needed'
+                                        New-UDSelectOption -Name 'Weekly' -Value 'weekly'
+                                    }
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 12 -SmallSize 2 -Children {
+                                    New-UDCheckbox -Id "med_reminders_$currentMedCount" -Label '🔔 Reminders'
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 2 -SmallSize 1 -Children {
+                                    New-UDButton -Text '🗑️' -Color secondary -Size small -OnClick {
+                                        try {
+                                            Show-UDToast -Message "Removing Medication #$currentMedCount" -Duration 2000
+                                            Clear-UDElement -Id "medication-item-$currentMedCount"
+                                        } catch {
+                                            Show-UDToast -Message "Error removing medication: $($_.Exception.Message)" -Duration 3000 -BackgroundColor red
+                                        }
+                                    } -Style @{ class = 'remove-btn' }
+                                }
+                            }
+                        }
+                    }
                 } -Style @{ class = 'add-btn' }
             }
 
@@ -338,8 +379,55 @@ $SettingsPage = New-UDApp -Content {
                 }
 
                 New-UDButton -Text '+ Add Another Pain Location' -Color success -OnClick {
-                    # TODO: Add logic to dynamically add pain location entries
-                    Show-UDToast -Message 'Pain location added!' -Duration 2000
+                    # Use session variable to track pain location count
+                    if (-not $Session:PainLocationCounter) { $Session:PainLocationCounter = 2 }
+                    $painCount = $Session:PainLocationCounter
+                    $Session:PainLocationCounter++
+                    
+                    Show-UDToast -Message "Adding Pain Location #$painCount" -Duration 2000
+                    
+                    # Add new pain location entry to the container
+                    Add-UDElement -ParentId 'pain-locations-container' -Content {
+                        $currentPainCount = $painCount  # Capture in local scope
+                        New-UDElement -Id "pain-location-item-$currentPainCount" -Tag 'div' -Attributes @{ class = 'pain-location-item' } -Content {
+                            New-UDGrid -Container -Children {
+                                New-UDGrid -Item -ExtraSmallSize 12 -SmallSize 4 -Children {
+                                    New-UDSelect -Id "pain_location_$currentPainCount" -Label 'Pain Location' -FullWidth -Option {
+                                        New-UDSelectOption -Name 'Lower Back' -Value 'lower_back'
+                                        New-UDSelectOption -Name 'Upper Back' -Value 'upper_back'
+                                        New-UDSelectOption -Name 'Neck' -Value 'neck'
+                                        New-UDSelectOption -Name 'Right Hip' -Value 'right_hip'
+                                        New-UDSelectOption -Name 'Left Hip' -Value 'left_hip'
+                                        New-UDSelectOption -Name 'Right Knee' -Value 'right_knee'
+                                        New-UDSelectOption -Name 'Left Knee' -Value 'left_knee'
+                                        New-UDSelectOption -Name 'Right Shoulder' -Value 'right_shoulder'
+                                        New-UDSelectOption -Name 'Left Shoulder' -Value 'left_shoulder'
+                                        New-UDSelectOption -Name 'Head/Headache' -Value 'head'
+                                        New-UDSelectOption -Name 'Other' -Value 'other'
+                                    }
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 10 -SmallSize 3 -Children {
+                                    New-UDTextbox -Id "pain_description_$currentPainCount" -Label 'Description (optional)' -FullWidth
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 6 -SmallSize 2 -Children {
+                                    New-UDCheckbox -Id "pain_chronic_$currentPainCount" -Label 'Chronic'
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 4 -SmallSize 2 -Children {
+                                    New-UDTextbox -Id "pain_baseline_$currentPainCount" -Label 'Baseline (0-10)' -Type 'number' -Value '0' -FullWidth
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 2 -SmallSize 1 -Children {
+                                    New-UDButton -Text '🗑️' -Color secondary -Size small -OnClick {
+                                        try {
+                                            Show-UDToast -Message "Removing Pain Location #$currentPainCount" -Duration 2000
+                                            Clear-UDElement -Id "pain-location-item-$currentPainCount"
+                                        } catch {
+                                            Show-UDToast -Message "Error removing pain location: $($_.Exception.Message)" -Duration 3000 -BackgroundColor red
+                                        }
+                                    } -Style @{ class = 'remove-btn' }
+                                }
+                            }
+                        }
+                    }
                 } -Style @{ class = 'add-btn' }
             }
 
@@ -388,8 +476,62 @@ $SettingsPage = New-UDApp -Content {
                 }
 
                 New-UDButton -Text '+ Add Another Activity' -Color success -OnClick {
-                    # TODO: Add logic to dynamically add activity entries
-                    Show-UDToast -Message 'Activity added!' -Duration 2000
+                    # Use session variable to track activity count
+                    if (-not $Session:ActivityCounter) { $Session:ActivityCounter = 2 }
+                    $activityCount = $Session:ActivityCounter
+                    $Session:ActivityCounter++
+                    
+                    Show-UDToast -Message "Adding Activity #$activityCount" -Duration 2000
+                    
+                    # Add new activity entry to the container
+                    Add-UDElement -ParentId 'activities-container' -Content {
+                        $currentActivityCount = $activityCount  # Capture in local scope
+                        New-UDElement -Id "activity-item-$currentActivityCount" -Tag 'div' -Attributes @{ class = 'activity-item' } -Content {
+                            New-UDGrid -Container -Children {
+                                New-UDGrid -Item -ExtraSmallSize 12 -SmallSize 2 -Children {
+                                    New-UDTextbox -Id "activity_name_$currentActivityCount" -Label 'Activity Name' -FullWidth
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 12 -SmallSize 2 -Children {
+                                    New-UDSelect -Id "activity_category_$currentActivityCount" -Label 'Category' -FullWidth -Option {
+                                        New-UDSelectOption -Name 'Cardio' -Value 'cardio'
+                                        New-UDSelectOption -Name 'Strength Training' -Value 'strength'
+                                        New-UDSelectOption -Name 'Flexibility/Stretching' -Value 'flexibility'
+                                        New-UDSelectOption -Name 'Physical Therapy' -Value 'physical_therapy'
+                                        New-UDSelectOption -Name 'Daily Living' -Value 'daily_living'
+                                        New-UDSelectOption -Name 'Sports' -Value 'sports'
+                                        New-UDSelectOption -Name 'Other' -Value 'other'
+                                    }
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 6 -SmallSize 2 -Children {
+                                    New-UDSelect -Id "activity_intensity_$currentActivityCount" -Label 'Intensity' -FullWidth -Option {
+                                        New-UDSelectOption -Name 'Light' -Value 'light'
+                                        New-UDSelectOption -Name 'Moderate' -Value 'moderate'
+                                        New-UDSelectOption -Name 'Vigorous' -Value 'vigorous'
+                                    } -DefaultValue 'moderate'
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 6 -SmallSize 2 -Children {
+                                    New-UDTextbox -Id "activity_duration_$currentActivityCount" -Label 'Duration (min)' -Type 'number' -Value '30' -FullWidth
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 10 -SmallSize 2 -Children {
+                                    New-UDSelect -Id "activity_frequency_$currentActivityCount" -Label 'Goal Frequency' -FullWidth -Option {
+                                        New-UDSelectOption -Name 'Daily' -Value 'daily'
+                                        New-UDSelectOption -Name 'Weekly' -Value 'weekly'
+                                        New-UDSelectOption -Name 'Monthly' -Value 'monthly'
+                                    } -DefaultValue 'weekly'
+                                }
+                                New-UDGrid -Item -ExtraSmallSize 2 -SmallSize 2 -Children {
+                                    New-UDButton -Text '🗑️' -Color secondary -Size small -OnClick {
+                                        try {
+                                            Show-UDToast -Message "Removing Activity #$currentActivityCount" -Duration 2000
+                                            Clear-UDElement -Id "activity-item-$currentActivityCount"
+                                        } catch {
+                                            Show-UDToast -Message "Error removing activity: $($_.Exception.Message)" -Duration 3000 -BackgroundColor red
+                                        }
+                                    } -Style @{ class = 'remove-btn' }
+                                }
+                            }
+                        }
+                    }
                 } -Style @{ class = 'add-btn' }
             }
 
@@ -417,13 +559,88 @@ $SettingsPage = New-UDApp -Content {
                 }
             }
 
-            # Save Settings Button
+            # Save Settings Button - Wrapped in Form for data collection
+            New-UDForm -Id 'settings-form' -Children {
+                # Hidden submit button (we'll trigger this programmatically)
+                New-UDElement -Tag 'button' -Attributes @{ 
+                    type = 'submit' 
+                    style = 'display: none;' 
+                    id = 'hidden-submit-btn' 
+                }
+            } -OnSubmit {
+                try {
+                    Import-Module UserManagement -Force
+                    $FormData = $EventData[0]
+                    
+                    Write-Information "Settings form data received: $($FormData | ConvertTo-Json -Depth 3)"
+                    
+                    # Collect medications (dynamic entries)
+                    $Medications = @()
+                    for ($i = 1; $i -le 10; $i++) {  # Check up to 10 medication entries
+                        $medName = $FormData["med_name_$i"]
+                        if (-not [string]::IsNullOrEmpty($medName)) {
+                            $Medications += @{
+                                name = $medName
+                                dosage = $FormData["med_dosage_$i"]
+                                frequency = $FormData["med_frequency_$i"]
+                                reminders = $FormData["med_reminders_$i"] -eq $true
+                            }
+                        }
+                    }
+                    
+                    # Collect pain locations (dynamic entries)
+                    $PainLocations = @()
+                    for ($i = 1; $i -le 10; $i++) {  # Check up to 10 pain location entries
+                        $painLocation = $FormData["pain_location_$i"]
+                        if (-not [string]::IsNullOrEmpty($painLocation)) {
+                            $PainLocations += @{
+                                location = $painLocation
+                                description = $FormData["pain_description_$i"]
+                                chronic = $FormData["pain_chronic_$i"] -eq $true
+                                baseline = [int]($FormData["pain_baseline_$i"] ?? 0)
+                            }
+                        }
+                    }
+                    
+                    # Collect activities (dynamic entries)
+                    $Activities = @()
+                    for ($i = 1; $i -le 10; $i++) {  # Check up to 10 activity entries
+                        $activityName = $FormData["activity_name_$i"]
+                        if (-not [string]::IsNullOrEmpty($activityName)) {
+                            $Activities += @{
+                                name = $activityName
+                                category = $FormData["activity_category_$i"]
+                                intensity = $FormData["activity_intensity_$i"]
+                                duration = [int]($FormData["activity_duration_$i"] ?? 30)
+                                frequency = $FormData["activity_frequency_$i"]
+                            }
+                        }
+                    }
+                    
+                    # Call New-UserHealthPreferences with collected data
+                    $PreferencesResult = New-UserHealthPreferences -Email $User -Timezone $FormData.timezone -TemperatureUnit $FormData.temperature_unit -WeightUnit $FormData.weight_unit -Theme $FormData.theme -TrackBloodPressure:($FormData.track_blood_pressure -eq $true) -BloodPressureTargetSystolic ([int]($FormData.bp_target_systolic ?? 120)) -BloodPressureTargetDiastolic ([int]($FormData.bp_target_diastolic ?? 80)) -TrackOxygen:($FormData.track_oxygen -eq $true) -OxygenTargetMin ([int]($FormData.o2_target_min ?? 95)) -TrackHeartRate:($FormData.track_heart_rate -eq $true) -HeartRateTargetResting ([int]($FormData.hr_target_resting ?? 70)) -TrackTemperature:($FormData.track_temperature -eq $true) -TrackWeight:($FormData.track_weight -eq $true) -TrackGlucose:($FormData.track_glucose -eq $true) -TrackMedications:($FormData.track_medications -eq $true) -TrackPain:($FormData.track_pain -eq $true) -TrackActivities:($FormData.track_activities -eq $true) -TrackSleep:($FormData.track_sleep -eq $true) -TrackMood:($FormData.track_mood -eq $true) -NotificationsEnabled:($FormData.notifications_enabled -eq $true) -CriticalAlerts:($FormData.critical_alerts -eq $true) -DailySummary:($FormData.daily_summary -eq $true) -WeeklyReport:($FormData.weekly_report -eq $true) -ReminderTime ($FormData.reminder_time ?? '09:00') -Medications $Medications -PainLocations $PainLocations -Activities $Activities
+                    
+                    if ($PreferencesResult.Success) {
+                        Show-UDToast -Message 'Health tracking settings saved successfully!' -Duration 3000 -BackgroundColor '#4caf50'
+                        Write-Information "Preferences saved successfully: $($PreferencesResult.Message)"
+                    } else {
+                        Show-UDToast -Message "Error saving settings: $($PreferencesResult.Message)" -Duration 5000 -BackgroundColor '#f44336'
+                        Write-Error "Failed to save preferences: $($PreferencesResult.Message)"
+                    }
+                    
+                } catch {
+                    $errorMsg = "Error processing settings: $($_.Exception.Message)"
+                    Write-Error $errorMsg
+                    Show-UDToast -Message $errorMsg -Duration 5000 -BackgroundColor '#f44336'
+                }
+            }
+            
+            # Visible Save Button
             New-UDGrid -Container -Children {
                 New-UDGrid -Item -ExtraSmallSize 12 -Children {
                     New-UDButton -Text '💾 Save Health Tracking Settings' -Color primary -Size large -FullWidth -OnClick {
-                        # TODO: Implement settings save logic here
-                        # Collect all form data and call [UserProfile]::SetUserPreferences()
-                        Show-UDToast -Message 'Settings saved successfully!' -Duration 3000 -BackgroundColor '#4caf50'
+                        # Trigger the hidden form submit to collect all data
+                        Invoke-UDJavaScript -JavaScript "document.getElementById('hidden-submit-btn').click();"
                     } -Style @{
                         marginTop = '30px'
                         padding = '16px 0'
