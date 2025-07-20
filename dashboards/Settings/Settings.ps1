f471ad9d6e4e78e6cf5f3deba5bfe1365e1c0f0e
justacurley <alexwmcurley@gmail.com>
2025-07-19 18:52:20 -0600

    clean up toast debugger

--- dashboards/Settings/Settings.ps1
+++ dashboards/Settings/Settings.ps1
@@ -92,10 +92,8 @@ $SettingsPage = New-UDApp -Content {
         Show-UDToast -Message "Redirecting to login page." -MessageColor Green -Duration 1000
         Invoke-UDRedirect -Url /login -Native
     } else {
-    #   Write-Information ($Session:UserData | Select U*, P* | ConvertTo-Json -Depth 99)
-        # Write-Information ($Session:UserData.Preferences | Convertto-json -depth 99)
       $PSDefaultParameterValues["gpf:JsonPreferences"] = $Session:UserData.Preferences
-      Show-UDToast -Message (gpf 'tracking.vitals.heart_rate.device' | Convertto-Json) -Duration 10000 -Persistent
+    #   Show-UDToast -Message (gpf 'tracking.vitals.heart_rate.device' | Convertto-Json) -Duration 10000 -Persistent
     }
     # Add custom CSS for settings form styling
     New-UDElement -Tag 'style' -Content {
