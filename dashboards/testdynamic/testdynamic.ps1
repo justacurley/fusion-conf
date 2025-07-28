5c199b9e18fa3166515c8847222d79b5134db5a4
justacurley <alexwmcurley@gmail.com>
2025-07-28 17:45:16 -0600

    try to redirect to updateentry with entryid as url parameter

--- dashboards/testdynamic/testdynamic.ps1
+++ dashboards/testdynamic/testdynamic.ps1
@@ -1,8 +1,8 @@
-﻿$Nav = New-UDList -Content {
-    New-UDListItem -Label "Test1" -Href '/test'  -Icon (New-UDIcon -Icon Cog -Size 1x)
-    New-UDListItem -Label "Test2" -Href '/test2'  -Icon (New-UDIcon -Icon Cog -Size 1x)
-    New-UDListItem -Label "Test3" -Href '/test3'  -Icon (New-UDIcon -Icon Cog -Size 1x)
-}
+﻿# $Nav = New-UDList -Content {
+#     New-UDListItem -Label "Test1" -Href '/test'  -Icon (New-UDIcon -Icon Cog -Size 1x)
+#     New-UDListItem -Label "Test2" -Href '/test2'  -Icon (New-UDIcon -Icon Cog -Size 1x)
+#     New-UDListItem -Label "Test3" -Href '/test3'  -Icon (New-UDIcon -Icon Cog -Size 1x)
+# }
 
 $Pages = @()
 
@@ -10,12 +10,12 @@ $Pages += New-UDPage -Name 'test' -url '/test/:id' -Content {
     "User ID: $id"
 }
 
-$Pages += New-UDPage -Name 'test2' -url '/test2' -Content {
+# $Pages += New-UDPage -Name 'test2' -url '/test2' -Content {
 
-}
+# }
 
-$Pages += New-UDPage -Name 'test3' -url '/test3' -Content {
+# $Pages += New-UDPage -Name 'test3' -url '/test3' -Content {
 
-}
+# }
 
-New-UDApp -Title 'Navigation' -Pages $Pages -Navigation $Nav -NavigationLayout permanent
+New-UDApp -Title 'ParamPage' -Pages $Pages #-Navigation $Nav -NavigationLayout permanent
