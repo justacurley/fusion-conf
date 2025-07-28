cb4335a461add0c52244e52b622bc9bc62c7e585
justacurley <alexwmcurley@gmail.com>
2025-07-28 17:46:47 -0600

    try to redirect to updateentry with entryid as url parameter

--- dashboards/testdynamic/testdynamic.ps1
+++ dashboards/testdynamic/testdynamic.ps1
@@ -1,8 +1,8 @@
-﻿$Nav = New-UDList -Content {
-    New-UDListItem -Label "Test1" -Href '/test'  -Icon (New-UDIcon -Icon Cog -Size 1x)
-    New-UDListItem -Label "Test2" -Href '/test2'  -Icon (New-UDIcon -Icon Cog -Size 1x)
-    New-UDListItem -Label "Test3" -Href '/test3'  -Icon (New-UDIcon -Icon Cog -Size 1x)
-}
+﻿# $Nav = New-UDList -Content {
+#     New-UDListItem -Label "Test1" -Href '/test'  -Icon (New-UDIcon -Icon Cog -Size 1x)
+#     New-UDListItem -Label "Test2" -Href '/test2'  -Icon (New-UDIcon -Icon Cog -Size 1x)
+#     New-UDListItem -Label "Test3" -Href '/test3'  -Icon (New-UDIcon -Icon Cog -Size 1x)
+# }
 
 $Pages = @()
 
@@ -18,4 +18,4 @@ $Pages += New-UDPage -Name 'test3' -url '/test3' -Content {
 
 }
 
-New-UDApp -Title 'Navigation' -Pages $Pages -Navigation $Nav -NavigationLayout permanent
+New-UDApp -Title 'Navigation' -Pages $Pages #-Navigation $Nav -NavigationLayout permanent
